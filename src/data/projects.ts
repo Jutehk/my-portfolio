fde04ad2e59f7aaf30c35baaa46556b24f4ecfb1
Jutehk <julianandunge54@gmail.com>
2025-05-05 01:29:37 +0300

    commit

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,24 +1,28 @@
+// src/data/projects.ts
+import dashboardImg from '../assets/admindashboard.png';
+import netflixImg from '../assets/netflixclone.jpeg';
+import ecommerceImg from '../assets/ecommerce.png';
+
 export const projects = [
   {
     title: "Admin Dashboard",
     description: "Responsive dashboard with analytics, charts and user roles",
-    image: "src/public/images/admindashboard.png",
+    image: dashboardImg,
     link: "https://github.com/Jutehk/Admin-Dashboard.git",
     demo: "https://jutehk.github.io/Admin-Dashboard/",
   },
   {
     title: "Netflix Clone",
-    description: "Full-stack Netflix UI clone with Firebase auth and movie streaming via TMDB API.",
-    image: "src/public/images/netflixclone.jpeg",
+    description: " Netflix UI clone with Firebase auth and movie streaming via TMDB API.",
+    image: netflixImg,
     link: "https://github.com/Jutehk/netflixclone.git",
     demo: "https://zetflixfront.netlify.app/",
   },
   {
     title: "E-commerce Website",
     description: "Modern e-commerce store with cart, checkout, product filters, and Stripe integration.",
-    image: "src/public/images/ecommerce.png",
+    image: ecommerceImg,
     link: "https://github.com/Jutehk/E-commerce-website.git",
     demo: "https://jutehk.github.io/E-commerce-website/",
   },
-  
 ];
